175ab7696ed7e08349aa95c6c41eff3969490ebd
Vladislav Gubin <49865020+Zeulze@users.noreply.github.com>
2022-12-14 12:18:28 +0700

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,98 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelectorAll("button");
+  let panelPrev = document.querySelector(".previous"),
+    panelCurr = document.querySelector(".current");
+
+  function setPanelPrev(name) {
+    panelPrev.textContent = `${panelCurr.textContent} ${name}`;
+    panelCurr.textContent = "0";
+  }
+
+  function calc(name) {
+    panelCurr.textContent = eval(
+      `${panelPrev.textContent} ${name} ${panelCurr.textContent}`
+    );
+    panelPrev.textContent = "";
+  }
+
+  function targetHandler(name) {
+    if (panelPrev.textContent === "") {
+      setPanelPrev(name);
+    } else {
+      calc("");
+      setPanelPrev(name);
+    }
+  }
+
+  btn.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      let target = e.target;
+
+      if (target.classList.contains("number")) {
+        if (panelCurr.textContent === "0")
+          panelCurr.textContent = `${target.textContent}`;
+        else if (
+          panelCurr.textContent === "Infinity" ||
+          panelCurr.textContent === "-Infinity"
+        ) {
+          panelCurr.textContent = `${target.textContent}`;
+        } else {
+          panelCurr.textContent += `${target.textContent}`;
+        }
+      }
+
+      //Можно было через try catch сделать
+
+      if (target.classList.contains("clear")) {
+        panelCurr.textContent = "0";
+        panelPrev.textContent = "";
+      }
+
+      if (target.classList.contains("multiply")) {
+        targetHandler("*");
+      }
+
+      if (target.classList.contains("sum")) {
+        targetHandler("+");
+      }
+
+      if (target.classList.contains("minus")) {
+        targetHandler("-");
+      }
+
+      if (target.classList.contains("divide")) {
+        targetHandler("/");
+      }
+
+      if (target.classList.contains("dot")) {
+        panelCurr.textContent += `${target.textContent}`;
+      }
+
+      if (target.classList.contains("equality")) {
+        calc("");
+      }
+
+      if (target.classList.contains("delete")) {
+        if (
+          panelCurr.textContent !== "" &&
+          panelCurr.textContent !== "Infinity" &&
+          panelCurr.textContent !== "-Infinity"
+        ) {
+          panelCurr.textContent = panelCurr.textContent.slice(
+            0,
+            panelCurr.textContent.length - 1
+          );
+        }
+        if (
+          panelCurr.textContent === "" ||
+          panelCurr.textContent === "-" ||
+          panelCurr.textContent === "Infinity" ||
+          panelCurr.textContent === "-Infinity"
+        )
+          panelCurr.textContent = "0";
+      }
+    });
+  });
+});
